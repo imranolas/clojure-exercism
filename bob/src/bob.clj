c7d48486ba5554ef1ea2c8185a9c5ac7195fbd1c
imranolas <imranolas@gmail.com>
2016-02-26 12:12:53 +0000

    Completed bob the teenager

---
+++ bob/src/bob.clj
@@ -0,0 +1,17 @@
+(ns bob
+    (:gen-class))
+
+(defn- silence? [input] (clojure.string/blank? input))
+(defn- has-letters? [input] (boolean (re-find #"[a-zA-Z]" input)))
+(defn- question? [input] (clojure.string/ends-with? input "?"))
+(defn- shouting? [input] (and
+                           (= input (clojure.string/upper-case input))
+                           (has-letters? input)))
+gs
+(defn response-for
+      [input]
+  (cond
+    (silence? input) "Fine. Be that way!"
+    (shouting? input) "Whoa, chill out!"
+    (question? input) "Sure."
+    :else "Whatever."))
